b97f52fea8cf70cdc99fef910a1d35845c978772
Pedro Onayquem <ponayquen@gmail.com>
2026-01-10 18:18:25 -0300

    fix: corrige exceção de saldo insuficiente

--- src/br/com/banco/exception/SaldoInsuficienteException.java
+++ src/br/com/banco/exception/SaldoInsuficienteException.java
@@ -1,7 +1,10 @@
 package br.com.banco.exception;
 
-public class SaldoInsuficienteException extends RuntimeException{
+public class SaldoInsuficienteException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
 	public SaldoInsuficienteException(String mensagem) {
-		super(mensagem);
-	}
+        super(mensagem);
+    }
 }
