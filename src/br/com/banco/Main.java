af1788077dda1b63a3718fc15c30f8a3f0b9b461
PedroOnayquen <pedroonayquen15@gmail.com>
2026-01-10 17:28:50 -0300

    fix: adiciona tratamento de erro ao saque no Main

--- src/br/com/banco/Main.java
+++ src/br/com/banco/Main.java
@@ -1,12 +1,22 @@
 package br.com.banco;
 
 import br.com.banco.model.*;
+import br.com.banco.exception.SaldoInsuficienteException;
 
 public class Main {
-	public static void main(String[] args) {
-		Cliente cliente = new Cliente("Pedro", "123.123.123-44");
-		
-		System.out.println(cliente.getNome());
-		System.out.println(cliente.getCpf());
-	}
+    public static void main(String[] args) {
+
+        Cliente cliente = new Cliente("Pedro", "123.456.789-00");
+        Conta conta = new ContaCorrente(1, cliente);
+
+        conta.depositar(10000);
+
+        try {
+            conta.sacar(50);
+        } catch (SaldoInsuficienteException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        System.out.println("Saldo atual: " + conta.getSaldo());
+    }
 }
